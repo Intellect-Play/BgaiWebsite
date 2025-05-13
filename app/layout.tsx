5a0a8d821f82e49a2ad73192065df7dcffde2ecb
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-13 16:03:16 +0400

    refactor: layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
 import Layout from "./_featured/Layout/Layout";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Providers from "./_provide/Providers";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Layout>{children}</Layout>
+        <Providers>
+          <Layout>{children}</Layout>
+        </Providers>
       </body>
     </html>
   );
