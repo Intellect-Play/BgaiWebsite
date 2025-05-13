cacabd3108f797a36f1f3c1cc3ec99d7850750af
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-13 17:03:00 +0400

    bugfix: sidebar

--- app/_featured/Header/Header.tsx
+++ app/_featured/Header/Header.tsx
@@ -9,45 +9,54 @@ import { Menu, X } from "lucide-react";
 import "./header.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
-import { toggleSidebar } from "@/redux/sidebar/sidebarSlice";
+import { setSidebarOpen, toggleSidebar } from "@/redux/sidebar/sidebarSlice";
+import { Instagram, Twitter, Linkedin, Youtube } from "lucide-react";
+import Sidebar from "../Sidebar/Sidebar";
 
 const Header: React.FC = () => {
   const pathname = usePathname();
   const isOpen = useSelector((state: RootState) => state.sidebar.isOpen);
   const dispatch = useDispatch();
   return (
-    <div className="w-full fixed top-0 left-0 bg-[#f4f4f4] h-[70px] flex items-center">
-      <div className="max-w-[1000px] mx-auto w-full flex justify-between items-center">
-        <div className="flex items-center h-[50px]">
-          <Image
-            src={Logo}
-            alt="Bgai_Logo"
-            className="h-full w-auto object-contain"
-          />
-        </div>
+    <>
+      <div className="w-full z-[1] fixed top-0 left-0 bg-[#f4f4f4] h-[70px] flex items-center">
+        <div className="max-w-[1000px] mx-auto w-full flex justify-between items-center">
+          <div className="flex items-center h-[50px]">
+            <Image
+              src={Logo}
+              alt="Bgai_Logo"
+              className="h-full w-auto object-contain"
+            />
+          </div>
 
-        <ul className="flex header__mobile gap-[30px]  h-full items-center">
-          {menuItems.map((item) => (
-            <li key={item.path}>
-              <Link
-                href={item.path}
-                className={`h-full flex items-center px-3 ${
-                  pathname === item.path ? "bg-[#f4d4]  rounded-md" : ""
-                }`}
-              >
-                {item.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
+          <ul className="flex header__mobile gap-[30px]  h-full items-center">
+            {menuItems.map((item) => (
+              <li key={item.path}>
+                <Link
+                  href={item.path}
+                  className={`h-full flex items-center px-3 ${
+                    pathname === item.path ? "bg-[#f4d4]  rounded-md" : ""
+                  }`}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
 
-        <div className="header__hamburger">
-          <button onClick={() => dispatch(toggleSidebar())}>
-            {isOpen ? <X size={30} /> : <Menu size={30} />}
-          </button>
+          <div className="header__hamburger">
+            <button
+              className="p-[15px] cursor-pointer"
+              onClick={() => dispatch(toggleSidebar())}
+            >
+              <Menu size={30} />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <Sidebar />
+    </>
   );
 };
 
---
+++ app/_featured/Sidebar/Sidebar.tsx
@@ -0,0 +1,92 @@
+"use client";
+import { RootState } from "@/redux/store";
+import {
+  Instagram,
+  Linkedin,
+  Twitter,
+  X as CloseIcon,
+  Youtube,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Logo from "../../_assets/images/peak.png";
+import { menuItems } from "@/app/constants/MenuItems";
+import { setSidebarOpen } from "@/redux/sidebar/sidebarSlice";
+import { motion } from "framer-motion";
+
+const Sidebar = () => {
+  const pathname = usePathname();
+  const isReduxOpen = useSelector((state: RootState) => state.sidebar.isOpen);
+  const dispatch = useDispatch();
+
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (isReduxOpen) {
+      setVisible(true);
+    }
+  }, [isReduxOpen]);
+
+  const handleClose = () => {
+    setVisible(false);
+  };
+
+  return (
+    <motion.div
+      initial={false}
+      animate={{ x: visible ? 0 : 350 }}
+      transition={{ type: "tween", duration: 0.4 }}
+      onAnimationComplete={() => {
+        if (!visible) {
+          dispatch(setSidebarOpen(false));
+        }
+      }}
+      className="fixed top-0 right-[0] z-10 h-full w-[350px] bg-[#212325] flex flex-col justify-between pointer-events-auto"
+    >
+      <div>
+        <div className="flex justify-between pt-[10px] px-[20px]">
+          <div className="flex items-center h-[50px]">
+            <Image
+              src={Logo}
+              alt="Bgai_Logo"
+              className="h-full w-auto object-contain"
+            />
+          </div>
+          <div className="flex items-center">
+            <button onClick={handleClose}>
+              <CloseIcon size={30} color="white" />
+            </button>
+          </div>
+        </div>
+        <div className="pt-[10px]">
+          <ul className="flex flex-col gap-[5px] text-[#fff]">
+            {menuItems.map((item) => (
+              <li key={item.path}>
+                <Link
+                  href={item.path}
+                  className={`h-full w-full flex items-center px-[20px] py-[10px] ${
+                    pathname === item.path ? "bg-[#363739] rounded-md" : ""
+                  }`}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="flex gap-[10px] justify-center items-center pb-[20px]">
+        <Instagram size={24} color="white" />
+        <Twitter size={24} color="white" />
+        <Linkedin size={24} color="white" />
+        <Youtube size={24} color="white" />
+      </div>
+    </motion.div>
+  );
+};
+
+export default Sidebar;
