220afe7b57de61975ddad280d6156615f222e81f
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-14 23:04:34 +0400

    chore: Positions section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import DiscoverNow from "./DiscoverNow/DiscoverNow";
 import AboutSection from "./AboutSection/AboutSection";
+import PositionsSection from "./PositionsSection/PositionsSection";
 
 const HomePage: React.FC = () => {
   return (
     <>
       <DiscoverNow />
       <AboutSection />
+      <PositionsSection backgroundImage="/images/guysimage.jpg" />
     </>
   );
 };
