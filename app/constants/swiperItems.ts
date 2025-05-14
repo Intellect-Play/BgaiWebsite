5716796a611ef981e9a8a0f60134e2e9444e47e8
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-14 20:39:02 +0400

    chore: About Section

---
+++ app/constants/swiperItems.ts
@@ -0,0 +1,9 @@
+export const swiperItems = [
+  { id: 1, image: "/images/swiperImages/swip1.jpg", alt: "Team working" },
+  {
+    id: 2,
+    image: "/images/swiperImages/swip2.jpg",
+    alt: "Brainstorming session",
+  },
+  { id: 3, image: "/images/swiperImages/swip3.jpg", alt: "Office vibes" },
+];
