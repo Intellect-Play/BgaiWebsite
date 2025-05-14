617fff346d74c7254ec7164eeabb442c63003391
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-14 15:03:18 +0400

    style: Navbar color changed

--- app/_featured/Header/Header.tsx
+++ app/_featured/Header/Header.tsx
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
   const dispatch = useDispatch();
   return (
     <>
-      <div className="w-full z-[1] fixed top-0 left-0 bg-[#f4f4f4] h-[70px] flex items-center">
+      <div className="w-full z-[10] fixed top-0 left-0 bg-transparent h-[70px] flex items-center">
         <div className="max-w-[1000px] mx-auto w-full flex justify-between items-center">
           <div className="flex items-center h-[50px]">
             <Image
@@ -29,13 +29,15 @@ const Header: React.FC = () => {
             />
           </div>
 
-          <ul className="flex header__mobile gap-[30px]  h-full items-center">
+          <ul className="flex header__mobile gap-[30px] text-[#fff] h-full items-center">
             {menuItems.map((item) => (
-              <li key={item.path}>
+              <li className=" " key={item.path}>
                 <Link
                   href={item.path}
-                  className={`h-full flex items-center px-3 ${
-                    pathname === item.path ? "bg-[#f4d4]  rounded-md" : ""
+                  className={`h-full flex items-center   py-[10px] px-[10px] px-3 ${
+                    pathname === item.path
+                      ? "bg-[#fff]/10 text-[#fff]   rounded-md"
+                      : ""
                   }`}
                 >
                   {item.title}
