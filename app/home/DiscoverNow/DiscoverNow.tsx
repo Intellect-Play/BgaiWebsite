01ed4cc84f983b4791ad05b78c707f1ef60dc2a5
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-14 15:47:17 +0400

    refactor: discover now

---
+++ app/home/DiscoverNow/DiscoverNow.tsx
@@ -0,0 +1,36 @@
+"use client";
+import CustomButton from "@/app/_components/CustomButton";
+import { ChevronDown } from "lucide-react";
+import React from "react";
+import "./discoverNow.scss";
+
+const DiscoverNow: React.FC = () => {
+  return (
+    <div className="relative w-full h-[600px]">
+      <video
+        className="w-full h-full object-cover"
+        src={"/videos/homePageVideo.mp4"}
+        autoPlay
+        loop
+        muted
+      />
+
+      <div className="absolute  top-[0] left-[0] w-full h-full bg-[rgba(0,0,0,0.5)] flex flex-col justify-center items-center text-white text-center">
+        <h1 className="text-4xl discoverFont text-[3.5rem] text-[#fff] max-w-[550px] mb-4">
+          People are at the <span className="font-[1000]">core</span> of
+          everything we do{" "}
+        </h1>
+
+        <CustomButton
+          title="Discover Now"
+          icon={<ChevronDown size={20} />}
+          width="12rem"
+          expandedWidth="12.5rem"
+          height="3rem"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DiscoverNow;
