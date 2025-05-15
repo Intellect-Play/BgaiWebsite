a478004d4a8f5d207c1b0c5dd1a2132c397d3f86
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-15 12:38:56 +0400

    chore: how we work reusable banner

--- app/constants/MenuItems.ts
+++ app/constants/MenuItems.ts
@@ -3,7 +3,7 @@ export const menuItems = [
   { title: "About Us", path: "/about" },
   { title: "Life at Peak", path: "/life-at-peak" },
   { title: "Games", path: "/games" },
-  { title: "How we work", path: "/how-we-work" },
+  { title: "How we work", path: "/howWeWork" },
   { title: "Internships", path: "/internships" },
   { title: "Jobs", path: "/jobs" },
 ];
