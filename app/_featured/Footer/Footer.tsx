edb3a1cf5850edf363160b5899686fb86c8a2fd4
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-15 15:45:55 +0400

    chore: footer

---
+++ app/_featured/Footer/Footer.tsx
@@ -0,0 +1,52 @@
+import { footerData } from "@/app/constants/footerItems";
+import { Instagram, Linkedin, Twitter, Youtube } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#212325] py-[50px]">
+      <div className="max-w-[1200px] mx-auto px-[20px] flex-col">
+        <div className="flex flex-wrap justify-between items-start gap-[20px]">
+          <div className="w-[200px]">
+            <Image
+              height={100}
+              width={100}
+              alt="logo"
+              src={"/images/peak.png"}
+            />
+          </div>
+
+          {footerData.map((section, i) => (
+            <div key={i} className="w-[200px]">
+              <h2 className="mb-[5px] text-[20px] text-[#fff] font-[600]">
+                {section.title}
+              </h2>
+              <ul className="flex flex-col gap-[2px] text-[14px] text-[#A3A4A8]">
+                {section.links.map((link, j) => (
+                  <li key={j}>
+                    <Link href={link.path}>{link.label}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-between border-t-[1px] border-t-[#A3A4A8]  mt-[30px] pt-[20px]  ">
+          <div className="text-[#A3A4A8]">
+            <p>© Peak. All Rights Reserved</p>
+          </div>
+          <div className="flex gap-[10px] justify-center items-center pb-[20px]">
+            <Instagram size={24} color="#F6F6FB" />
+            <Twitter size={24} color="#F6F6FB" />
+            <Linkedin size={24} color="#F6F6FB" />
+            <Youtube size={24} color="#F6F6FB" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ app/constants/footerItems.ts
@@ -0,0 +1,28 @@
+export const footerData = [
+  {
+    title: "Company",
+    links: [
+      { label: "About Us", path: "/about" },
+      { label: "Life at Peak", path: "/life-at-peak" },
+      { label: "Games", path: "/games" },
+      { label: "How We Work", path: "/howWeWork" },
+      { label: "Internships", path: "/internships" },
+      { label: "Jobs", path: "/jobs" },
+    ],
+  },
+  {
+    title: "Links",
+    links: [
+      { label: "Art Station", path: "/art-station" },
+      { label: "Press", path: "/press" },
+      { label: "Contact", path: "/contact" },
+    ],
+  },
+  {
+    title: "Terms & Privacy",
+    links: [
+      { label: "Terms & Privacy (Product)", path: "/terms-product" },
+      { label: "Privacy (Job Application)", path: "/privacy-jobs" },
+    ],
+  },
+];
