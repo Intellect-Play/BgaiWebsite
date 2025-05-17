c05cc5ef2c19197909f06880401c6d291aa07f79
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-17 21:01:01 +0400

    style: accardion

--- app/lifeatpeak/Accardion/Accardion.tsx
+++ app/lifeatpeak/Accardion/Accardion.tsx
@@ -4,11 +4,31 @@ import { motion, AnimatePresence } from "framer-motion";
 import "./accardion.scss";
 
 const items = [
-  "Learning & Development",
-  "Compensation",
-  "Travels",
-  "Health",
-  "Relocation",
+  {
+    title: "Learning & Development",
+    description:
+      "Keep on learning. We are frequently attending relevant conferences and have several on-demand learning platforms at your disposal. If you need anything on top of that let us know - we’ll make it ready for you.",
+  },
+  {
+    title: "Compensation",
+    description:
+      "Only a free mind can do great things. Our compensation leaves no room for worries.",
+  },
+  {
+    title: "Travels",
+    description:
+      "Broaden your horizon together with your team during various trips and events. Some teams link this to a conference, some just choose a country they want to explore together.",
+  },
+  {
+    title: "Health",
+    description:
+      "The most precious thing we have is our health. We provide you, your spouse and your children with a fully-paid private insurance package to ensure you’re covered in case you need it. Feel free to also take advice or a second opinion from our company doctor.",
+  },
+  {
+    title: "Relocation",
+    description:
+      "We offer a relocation package that makes life easier for you while relocating and making yourself comfy in Istanbul – one of the most vibrant and cosmopolitan cities in the world.",
+  },
 ];
 
 const Accardion = () => {
@@ -19,41 +39,41 @@ const Accardion = () => {
   };
 
   return (
-    <div className="benefits-wrapper">
-      <div className="benefits-left">
-        <h2>
-          Perks <span>&</span> Benefits
-        </h2>
-      </div>
+    <div className="bg-[#F9F9F9] py-[30px]">
+      <div className="benefits-wrapper">
+        <div className="benefits-left">
+          <h2 className="font-[600]">
+            Perks <span>&</span> Benefits
+          </h2>
+        </div>
 
-      <div className="benefits-right">
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className={`accordion-item ${openIndex === index ? "open" : ""}`}
-          >
-            <div className="accordion-title" onClick={() => toggle(index)}>
-              {item}
-              <span className="icon">{openIndex === index ? "▲" : "▼"}</span>
+        <div className="benefits-right">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className={`accordion-item ${openIndex === index ? "open" : ""}`}
+            >
+              <div className="accordion-title" onClick={() => toggle(index)}>
+                {item.title}
+                <span className="icon">{openIndex === index ? "▲" : "▼"}</span>
+              </div>
+
+              <AnimatePresence initial={false}>
+                {openIndex === index && (
+                  <motion.div
+                    className="accordion-content"
+                    initial={{ height: 0, opacity: 0 }}
+                    animate={{ height: "auto", opacity: 1 }}
+                    exit={{ height: 0, opacity: 0 }}
+                    transition={{ duration: 0.3 }}
+                  >
+                    <p>{item.description}</p>
+                  </motion.div>
+                )}
+              </AnimatePresence>
             </div>
-
-            <AnimatePresence initial={false}>
-              {openIndex === index && (
-                <motion.div
-                  className="accordion-content"
-                  initial={{ height: 0, opacity: 0 }}
-                  animate={{ height: "auto", opacity: 1 }}
-                  exit={{ height: 0, opacity: 0 }}
-                  transition={{ duration: 0.3 }}
-                >
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
