d92efdde0a8c8cdb1900266c183a8ec8bd609f8b
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-17 16:31:13 +0400

    focus section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/_components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/_utils/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       keyframes: {
