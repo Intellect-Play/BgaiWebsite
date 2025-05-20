06aeadc40d8a34546c4cadd0c4d7091c53a092d3
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-20 15:52:28 +0400

    backend

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,26 @@ const app = express();
 const cors = require("cors");
 const path = require("path");
 const router = express.Router();
+const connection = require("./db/db");
+app.use(express.static(__dirname));
+
+connection();
+
+const gameRouter = require("./routes/gameRoutes");
+
+console.log("Mongo URL:", process.env.DB);
+
+//middlewares
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use("/api/bgaiv1/games", gameRouter);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}`));
